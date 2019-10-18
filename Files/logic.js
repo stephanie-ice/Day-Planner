362aaa2b689259b5b3a93af12d6ba6a042e201c4
Stephanie Ice <stephanie.icee@gmail.com>
2019-10-18 13:59:48 -0700

    saveBtn saves to local storage but saves and returns empty

--- Files/logic.js
+++ Files/logic.js
@@ -2,28 +2,35 @@ console.log ("The logic page is linked correctly");
 
 $(document).ready(function(){
     alert("JQ working");
- });
-/*This is the logic for the save button for each hour*/
-var saveBtn = $(".saveBtn");
-var userInput = $("#input");
-var theHour = $("#9AM");
-/*click event that*/
-saveBtn.on("click", function () {
-/*if input is not empty or null, then*/
-    if (userInput !== null) {
-/*grabs input from field*/
-        userInput.val ();
-        console.log (userInput.val());
-/*grabs the time id of the time field*/
-        /*maybe not yet? grab somewhere else, like at the end*/
-/*converts input text to an array 
-saves to local storage
-displays in the field*/
-    };
+    /*This is the logic for the save button for each hour*/
+    var saveBtn = $("#saveBtn");
+    var userInput = $("#input");
+    /*click event that*/
+    saveBtn.on("click", function () {
+        alert("The click button is working");
+        /*if input is not empty or null, then*/
+        if (userInput !== null) {
+            /*grabs input from field*/
+            var input = userInput.text();
+            console.log (input);
+
+            event.preventDefault();
+            
+            /*saves to local storage*/
+            localStorage.setItem("9AM", JSON.stringify(input));
+            /*displays in the field*/
+            var savedInput = localStorage.getItem("9AM");
+            userInput.textContent= savedInput;
+            console.log (savedInput);    
+            }
+        else {
+            alert("Please, tell us all about your busy life!");
+        };    
+    });
+    /*This is the logic to check moment.js for the current time*/
+    /*compares current time to input field id
+    if value of id < current hour, past
+    if value of id = current hour, present (minutes not considered)
+    if value of id > current hour, future
+    toggle the class style based on the comparison outcome*/
 });
-/*This is the logic to check moment.js for the current time*/
-/*compares current time to input field id
-if value of id < current hour, past
-if value of id = current hour, present (minutes not considered)
-if value of id > current hour, future
-toggle the class style based on the comparison outcome*/
