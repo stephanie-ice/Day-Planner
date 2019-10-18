a024c56d7a4ea874c01f961124df6965f1de4bb0
Stephanie Ice <stephanie.icee@gmail.com>
2019-10-18 12:02:36 -0700

    working JS page and JQ as well

--- Files/logic.js
+++ Files/logic.js
@@ -1,16 +1,29 @@
-/*This is the logic for the submit button for each hour*/
+console.log ("The logic page is linked correctly");
 
-/*click event that
-if input is not empty or null, then
-grabs input from field
-notes the id of the field, set to the corresponding hour
-converts input text to an array 
+$(document).ready(function(){
+    alert("JQ working");
+ });
+/*This is the logic for the save button for each hour*/
+var saveBtn = $(".saveBtn");
+var userInput = $("#input");
+var theHour = $("#9AM");
+/*click event that*/
+saveBtn.on("click", function () {
+/*if input is not empty or null, then*/
+    if (userInput !== null) {
+/*grabs input from field*/
+        userInput.val ();
+        console.log (userInput.val());
+/*grabs the time id of the time field*/
+        /*maybe not yet? grab somewhere else, like at the end*/
+/*converts input text to an array 
 saves to local storage
 displays in the field*/
-
-/*This is the logic to check moment.js for the current time
-compares current time to input field id
+    };
+});
+/*This is the logic to check moment.js for the current time*/
+/*compares current time to input field id
 if value of id < current hour, past
 if value of id = current hour, present (minutes not considered)
 if value of id > current hour, future
-toggle the class style based on the comparison outcome
+toggle the class style based on the comparison outcome*/
