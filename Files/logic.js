b2d1aa9387ef14c10eb0a91033503c6ba89e03c7
Stephanie Ice <stephanie.icee@gmail.com>
2019-10-19 20:08:06 -0700

    got help with the save button functionality

--- Files/logic.js
+++ Files/logic.js
@@ -1,31 +1,34 @@
 console.log ("The logic page is linked correctly");
 
 $(document).ready(function(){
-    alert("JQ working");
+    console.log("JQ working");
     /*This is the logic for the save button for each hour*/
-    var saveBtn = $("#saveBtn");
-    var userInput = $("#input");
     /*click event that*/
-    saveBtn.on("click", function () {
-        alert("The click button is working");
-        /*if input is not empty or null, then*/
-        if (userInput !== null) {
-            /*grabs input from field*/
-            var input = userInput.text();
-            console.log (input);
+    /*I got help with this; the .key method is new learning for me. It helps me to make the hour value a variable
+    sot hat I don't have to make 8 click events!!!*/
 
-            event.preventDefault();
-            
+    for (var i = 0; i < localStorage.length; ++i ) {
+        console.log("key", localStorage.key(i)) 
+        console.log("value", localStorage.getItem(localStorage.key(i)) );
+        $("#task_" + localStorage.key(i)).val(localStorage.getItem(localStorage.key(i)));
+    }    
+
+    $(".saveBtn").on("click", function (event) {
+        event.preventDefault();
+
+        var time = $(event.target).data("time");
+        var task = $("#task_" + time).val();
+
+        console.log("The click button is working");
+        /*if input is not empty or null, then*/
+        if (task !== "") {
+            console.log(task);
+
             /*saves to local storage*/
-            localStorage.setItem("9AM", JSON.stringify(input));
-            /*displays in the field*/
-            var savedInput = localStorage.getItem("9AM");
-            userInput.textContent= savedInput;
-            console.log (savedInput);    
-            }
-        else {
+            localStorage.setItem(time, task);
+        } else {
             alert("Please, tell us all about your busy life!");
-        };    
+        }  
     });
     /*This is the logic to check moment.js for the current time*/
     /*compares current time to input field id
